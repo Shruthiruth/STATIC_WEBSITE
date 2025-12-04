0bb0e072da3926fc6e41694273a0791962c1f649
Shruthiruth <shruthiruth21@gmail.com>
2025-12-04 10:31:42 +0530

    sample directory push

---
+++ test.java
@@ -0,0 +1,13 @@
+class test
+{
+
+    
+public void m1()
+{
+    System.out.println("sample directory");
+}
+    public static void main(String[] args) {
+            test t= new test();
+            t.m1();
+    }
+}
